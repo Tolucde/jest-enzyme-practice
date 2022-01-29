021621cc80d41673759b1f5a074a401e140e1bd3
Tolu Philip <toluphilip619@gmail.com>
2022-01-29 21:52:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(1)
   return (
-    <div style={{ padding: '100px' }}>
-      <div style={{ fontSize: '50px' }} className='counter-display'>
+    <div data-test='component-app' style={{ padding: '100px' }}>
+      <div
+        data-test='counter-display'
+        style={{ fontSize: '50px' }}
+        className='counter-display'>
         {counter}
       </div>
-      <button>INCREASE</button>
+      <button
+        onClick={() => setCounter(counter + 1)}
+        data-test='button-increase'>
+        INCREASED
+      </button>
+      <button
+        onClick={() => setCounter(counter - 1)}
+        data-test='button-decrease'>
+        DECREASED
+      </button>
     </div>
   )
 }
